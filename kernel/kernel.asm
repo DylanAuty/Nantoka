b2c0c4f7f896f042735fda09fd0176e42d33932f
Dylan Auty <dylan.auty@gmail.com>
2015-08-30 15:47:28 +0100

    added read_port and write_port functions

--- kernel/kernel.asm
+++ kernel/kernel.asm
@@ -29,3 +29,26 @@ start:
 section .bss
 resb 8192		; Set aside 8kb for the stack, resb is REServe memory in Bytes
 stack_space:
+
+section .portio
+
+read_port:				# read_port will take the port number as an argument
+	mov edx, [esp+4]	# When calling functions with arguments, the arguments get shoved onto the stack
+						# Reading them means using the stack pointer to fetch them, and put them in the edx register.
+						# At this point we can work with the input argument. Adding 4 because we're at the top of the stack.
+	in al, dx			# dx is the lower 16 bits of register edx.
+						# in reads the port whose number is in the second argument - in this case dx.
+						# The result goes into al. Note this is different to mov, as dx is not the value - it's the value at
+						# port number dx that we want.
+						# al is the lower 8 bits of register eax - eax is used to provide returns from functions - same as writing
+						# 'return value' at the end of a c function call and read as such by a c function.
+	ret					# go back wherever this is from
+
+write_port:				# Takes 2 arguments, port number and data to be written. Function 'out' writes to port at given address.
+	mov edx, [esp + 4]	# Argument 1 is port number
+	mov al, [esp + 8]	# al is lower 16 bits of EAX - for returns from functions but also for arithmetic, interrupt calls, etc...
+						# Don't think there's any reason to use eax over others, since we're not trying to return anything
+						# Probably just for convenience's sake?
+	out dx, al			# Write the argument (stored in al) to the port at dx
+	ret
+
